8e3206dfd695d1c2761ae414ff4ab74b5e929e11
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-25 17:31:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = {prop : 1}
+var recipes = {prop : 1, prop2:2}
 function updateObjectWithKeyAndValue(object, key, value) {
   recipes[key] = value
   return recipes
@@ -13,7 +13,15 @@ function deleteFromObjectByKey(object, key){
    delete newObject[key]
    return newObject
 }
+var z = deleteFromObjectByKey(recipes,"prop")
+
+//console.log(z)
+//console.log(recipes)
+
+
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
+var z = destructivelyDeleteFromObjectByKey(recipes,"prop")
+console.log(recipes)
